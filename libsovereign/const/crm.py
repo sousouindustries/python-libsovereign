8d1aae3e48078b3498c3a3950b028298b56cd056
Cochise Ruhulessin <cochiseruhulessin@gmail.com>
2016-01-12 06:25:37 +0100

    Initial commit

---
+++ libsovereign/const/crm.py
@@ -0,0 +1,12 @@
+
+
+#: Represents a party of type Organization
+ORG_T = 'O'
+
+
+#: Represents a party of type Person.
+IND_T = 'P'
+
+
+#: Specifies all valid party types.
+VALID_PARTY_TYPES = [ORG_T, IND_T]
---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+import sys
+from distutils.sysconfig import get_python_lib
+
+from setuptools import find_packages, setup
+
+EXCLUDE_FROM_PACKAGES = []
+MODULE_NAME = 'libsovereign'
+
+
+# Dynamically calculate the version based,
+version = __import__(MODULE_NAME).get_version()
+
+
+setup(
+    name=MODULE_NAME,
+    version=version,
+    url='https://www.sousouindustries.com/',
+    author='Cochise Ruhulessin',
+    author_email='cochise.ruhulessin@sousouindustries.com',
+    description=("A Python shared library for the Sovereign system."),
+    license='Apache Licence 2.0',
+    packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
